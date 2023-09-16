8339d2243b132fcbbc3ecdee6b2337db456a79ec
Justine Liene Laumane <justinellaumane@gmail.com>
2023-09-16 10:40:48 +0300

    👌 IMPROVE: Exercise 3

--- 03/README.md
+++ 03/README.md
@@ -1,1 +1,37 @@
-# To be improved
+# Exercise 3
+## Demonstrate the use of the following Python data types
+1. List
+```python
+people = ["Anna", "Baiba", "Dairis"] # initialize
+people.pop() # remove last element
+people[0] # access element
+```
+2. Dictionary
+```python
+people_ages = {
+    "Anna": 35,
+    "Baiba": 25,
+    "Dairis": 100
+} # initialize
+
+people_ages.pop("Anna")  # remove
+people_ages["Sandra"] = 55  # add
+people_ages["Baiba"] # print value for key "Baiba" 
+```
+3. Set
+```python
+set1 = {"Windows", "Linux", "Mac"} # initialize in a hard coded way way
+set2 = set(["Windows", "Linux", "Mac", "Linux"]) # initialize from list
+set1.add("Android") # add
+union_set = set1.union(set2) # merge
+intersection_set = set1.intersection(set2) # the common values
+difference_set = set1.difference(set2) # the unique values in set1
+```
+4. Tuple
+```python
+new_person = ("Edijs", 45) # initialize
+name, age = new_person # access/unpack elements
+new_people = new_person1 + new_person2 # merge tuples
+```
+
+### At the end of the main.py file there are more examples/experiments with these data types.
--- 03/main.py
+++ 03/main.py
@@ -1,29 +1,80 @@
 # List
+print("--------------Some examples with lists------------------")
 people = ["Anna", "Baiba", "Dairis"]
 
 print(people)
-print("Last person in the list is: ",people.pop())
-print("First person in the list is: ",people[0])
+print("Last person in the list is: ", people.pop()) # remove and print the last
+print("First person in the list is: ", people[0])
 
 # Dictionary
+print("------------Some examples with dictionaries--------------")
 people_ages = {
     "Anna": 35,
     "Baiba": 25,
     "Dairis": 100
 }
 
+people_ages.pop("Anna")  # remove
+people_ages["Sandra"] = 55  # add
 print(people_ages)
-print("Baiba is ",people_ages["Baiba"], " years old")
+print("Baiba is ", people_ages["Baiba"], " years old")
 
 # Set
+print("--------------Some examples with sets------------------")
 set1 = {"Windows", "Linux", "Mac"}
 # same as
 set2 = set(["Windows", "Linux", "Mac", "Linux"])
-print(set1)
-print(set2)
+print(set1, "=",set2)
+set1.add("Android")
+print("Updated set:",set1)
+
+union_set = set1.union(set2)
+print("Union of set1 and set2:", union_set)
+
+intersection_set = set1.intersection(set2)
+print("Intersection of set1 and set2:", intersection_set)
+
+difference_set = set1.difference(set2)
+print("Elements in set1 but not in set2:", difference_set)
 
 # Tuple
-new_person = ("Edijs", 45)
-name, age = new_person
+print("-------------Some examples with tuples-----------------")
+
+new_person1 = ("Edijs", 45)
+name, age = new_person1
+new_person2 = ("Sandra", 26)
+new_people = new_person1 + new_person2
 
 print(f"The new employee {name} is {age} years old.")
+print("All new people: ", new_people)
+
+# Interactions
+print("-------------Some interactions between the types and cooler examples-----------------")
+
+# Check the age of everyone in the list
+print("There are ",len(people), " people in the list.")
+for person in people:
+    if person in people_ages:
+        age = people_ages[person]
+        print(f"{person} is {age} years old.")
+    else:
+        print(f"{person} is unknown.")
+
+
+# Creating a new list of people's names from the dictionary keys
+print("------------")
+people_names = list(people_ages.keys())
+print("List of people's names from the dictionary:", people_names)
+
+# Getting the average age of people in the dictionary
+print("------------")
+age_sum = 0
+people_count = 0
+
+for age in people_ages.values():
+    age_sum += age
+    people_count += 1
+
+if people_count > 0:
+    average_age = age_sum / people_count
+    print(f"The average age of the {people_count} people in the dictionary is {average_age:.0f} years.")
