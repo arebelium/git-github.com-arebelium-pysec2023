0581ab5c90d1e78df0547913e82926e2fe3e3978
Justine Liene Laumane <justinellaumane@gmail.com>
2023-09-16 11:57:28 +0300

    👌 IMPROVE: Exercise 4 PT.2

--- 04/README.md
+++ 04/README.md
@@ -1,6 +1,7 @@
 # Exercise 4 
 ## Demonstrate the use of all types of loops in python:
 1. While Loop <br>
-Mini password cracker simulator which checks a set of passwords against a randomly chosen password from this set.
-2. For Loop
+`Mini password cracker simulator which checks a set of passwords against a randomly chosen password from this set.`
+2. For Loop <br>
+`Password strength checker - user enters his password and the program tells if it matches the criteria (lenght (>7), has upper, lower characters, numbers and special characters).`
 3. Range Loop
--- 04/main.py
+++ 04/main.py
@@ -1,7 +1,7 @@
 import random
 
 # While loop - mini password cracker simulator
-print("----Mini password cracker----")
+print("-------------Mini password cracker-------------")
 possible_passwords = ["12345", "password", "letmein", "admin", "secure123"]
 password = possible_passwords[random.randint(0,len(possible_passwords)-1)]
 
@@ -20,4 +20,33 @@ if not cracked:
 
 
 # For loop - password strength checker
-print("----Mini strength checker----")
+print("-----------Password strength checker-----------")
+input_password = input("Please enter a password:")
+print("------------------------")
+long_enough = len(input_password)>7
+upper_letters = False
+lower_letters = False
+digit = False
+special_characters = False
+allowed_special_characters = "!@#$%^&*?"
+all_allowed_characters = True
+
+for character in input_password:
+    if character.isupper():
+        upper_letters=True
+    elif character.islower():
+        lower_letters=True
+    elif character.isnumeric():
+        digit=True
+    elif character in allowed_special_characters:
+        special_characters = True
+    else:
+        all_allowed_characters = False
+        print("The password contains unallowed special characters. The allowed characters are:",special_characters)
+        break
+
+if (long_enough and upper_letters and lower_letters and special_characters and all_allowed_characters):
+    print("Good job! Your password matches the criteria!")
+else:
+    print("Unfortunately, your password doesn't match the criteria. \nIt should contain: \n- at least 8 characters, \n- both upper and lower letters, \n- digits, \n- special characters.")
+
